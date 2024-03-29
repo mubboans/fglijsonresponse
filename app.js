593ad6905ff0cadb7097424fc22ca9effeecfc3e
Mubashir <mubashir.ansari@digitalsalt.in>
2024-03-29 16:52:43 +0530

    added the blank route

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const port = 3002;
 const carddetail = require('./res/json/card.json')
 app.use(cors());
-
+route.get('/', (req, res) => {
+    res.send({ message: 'working api', status: true }).statusCode(200)
+})
 route.get('/dashboard/card', (req, res) => {
     res.send(carddetail)
 })
@@ -29,5 +31,6 @@ route.get('/dashboard/nca/detail', (req, res) => {
 // app.listen(port, () => {
 //     console.log(`Example app listening on port ${port}`)
 // })
+
 app.use('/.netlify/functions/api', route)
 module.exports = app;
